a8d2762db2893397dbe33f35a3951e9fde355cc3
Anshu Sinha <anshujuly2@gmail.com>
2023-04-13 19:41:37 +0530

    Add a code to display the question text on the GUI

--- ui.py
+++ ui.py
@@ -4,10 +4,14 @@ FONT = ("Arial", 20, "italic")
 """imported every class from the tkinter module"""
 from tkinter import *
 
+"""imported QuizBrain from the quiz_brain module"""
+from quiz_brain import QuizBrain
+
 """created QuizInterface class"""
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
 
         # window
 
@@ -27,7 +31,7 @@ class QuizInterface:
 
         """created the canvas object"""
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
-        self.questionText = self.canvas.create_text(150, 125, text="Hello", font=FONT, fill=THEME_COLOR)
+        self.questionText = self.canvas.create_text(150, 125, width=280, text="Hello", font=FONT, fill=THEME_COLOR)
         self.canvas.grid(row=2, column=1, columnspan=2, pady=50)
 
         # button
@@ -42,5 +46,12 @@ class QuizInterface:
         self.buttonFalse = Button(image=self.falseImg, highlightthickness=0, bd=0)
         self.buttonFalse.grid(row=3, column=2)
 
+        """calling getNextQuestion method"""
+        self.getNextQuestion()
+
         """loop to keep the window open"""
         self.window.mainloop()
+
+    def getNextQuestion(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.questionText, text=q_text)
